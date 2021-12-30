4e7e544eca8075235fee6fdfd047d841628f08a9
prabjot <prybinfo@gmail.com>
2021-12-30 19:18:05 +0530

    initial commit

--- lambdafunctiondeploy.py
+++ lambdafunctiondeploy.py
@@ -1,8 +1,18 @@
 import boto3
 import os
 import shutil
-cwd = os.getcwd()
-file_to_zip = 'lambda-zip.py'
-shutil.make_archive('lambda-zip','zip',cwd,file_to_zip)
-lambda_upload = boto3.client('s3')
-res = lambda_upload.upload_file('lambda-zip.zip', 'lambda-bucket-zip1', 'lambda-zip.zip')
+class ziper():
+    def zipping(self):
+        cwd = os.getcwd()
+        file_to_zip = 'lambda-zip.py'
+        shutil.make_archive('lambda-zip','zip',cwd,file_to_zip)
+class ld():
+    def lc(self):
+        lambda_upload = boto3.client('s3')
+        bucket_code = lambda_upload.create_bucket(
+            Bucket='codebucket-infy-lam',
+            CreateBucketConfiguration={
+                'LocationConstraint': 'ap-south-1'
+            },
+        )
+        res = lambda_upload.upload_file('lambda-zip.zip', 'lambda-bucket-zip1', 'lambda-zip.zip')
