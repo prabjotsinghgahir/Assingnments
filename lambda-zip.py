954156838ace468dee106b8cda4601daea256356
prabjot <prybinfo@gmail.com>
2022-01-04 21:54:38 +0530

    initial commit

--- lambda-zip.py
+++ lambda-zip.py
@@ -1,9 +1,11 @@
 import boto3
+import os
 s = boto3.client('s3')
 def handler(event, context):
+    destbucket = os.getenv('takebucket')
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = event['Records'][0]['s3']['object']['key']
-    dest_bucket_name = 'output-b-one'
+    dest_bucket_name = destbucket
     copy_object = {'Bucket':source_bucket, 'Key':file_name}
     s.copy_object(CopySource=copy_object,Bucket = dest_bucket_name,Key = file_name )
     print("Helooo fromm  me")
