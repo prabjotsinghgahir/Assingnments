99533e31184b783680c94b411801c92c0e54de44
prabjot <prybinfo@gmail.com>
2022-01-10 22:46:14 +0530

    initial commit

--- PythonDeploy_Code.py
+++ PythonDeploy_Code.py
@@ -1,6 +1,4 @@
-#import lambdafunctiondeploy
 import boto3
-
 
 client = boto3.client('cloudformation')
 
--- main.py
+++ main.py
@@ -45,35 +45,3 @@ for i in range(0, a):
     print("Done calling lambda deployer")
     PythonDeploy_Code.StackCreation().create_stack(stack_name, reading, call_parameters())
     PythonDeploy_Code.StackCreation().stackstatus(stack_name)
-
-'''parameter = [
-    {
-        'ParameterKey': 'S3Bucketname',
-        'ParameterValue': source_bucket_name
-    },
-    {
-        'ParameterKey': 'S3Destbucket',
-        'ParameterValue': destination_bucket_name
-    },
-    {
-        'ParameterKey': 'LambdaCodeBucket',
-        'ParameterValue': lambda_code_bucket
-    },
-    {
-        'ParameterKey': 'LambdaKey',
-        'ParameterValue': file_zip.split('.')[0]+".zip"
-    },
-    {
-        'ParameterKey': 'LambdaHandler',
-        'ParameterValue': file_zip.split('.')[0]+".handler"
-    }
-]'''
-
-
-'''opening_temp = open("Week_1_final")
-reading = opening_temp.read()
-file_zip = 'lambda-zip.py'
-lambda_code_bucket = 'codebucket-infy-lam'
-stack_name = 'assnig'
-source_bucket_name = 'source-bucket-psg'
-destination_bucket_name = 'desti-bucket-psg'''
