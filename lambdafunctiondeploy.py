23f353963d9a114c271e488c8ba75b60a655455c
prabjot <prybinfo@gmail.com>
2022-01-10 11:56:03 +0530

    initial commit

--- lambdafunctiondeploy.py
+++ lambdafunctiondeploy.py
@@ -3,14 +3,14 @@ import os
 import shutil
 
 
-class ziper():
+class ziper:
     def zipping(self, file_zip):
-        cwd = os.getcwd()
+        cwd = os.getcwd() + "\lambda_zips"
         file_to_zip = file_zip.split('.')
         shutil.make_archive(file_to_zip[0], 'zip', cwd, file_zip)
 
 
-class LambdaFunction():
+class LambdaFunction:
     def lambdafunctionupload(self, lambda_code_bucket, file_zip):
         lambda_upload = boto3.client('s3')
         try:
@@ -25,4 +25,5 @@ class LambdaFunction():
         file_zip = file_zip.split('.')
         file_zip_one = file_zip[0] + ".zip"
         res = lambda_upload.upload_file(file_zip_one, lambda_code_bucket, file_zip_one)
+
 
