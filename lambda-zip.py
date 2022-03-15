dcb81ac6dda152fd25deb24080f2a905dce9b205
prabjot <prybinfo@gmail.com>
2022-03-15 12:58:01 +0530

    added waiter exception

--- lambda-zip.py
+++ lambda-zip.py
@@ -7,7 +7,6 @@ def handler(event, context):
     destination_bucket = os.getenv('takeBucket')
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = event['Records'][0]['s3']['object']['key']
-    destination_bucket_name = destination_bucket
     copy_object = {'Bucket': source_bucket, 'Key': file_name}
-    s.copy_object(CopySource=copy_object, Bucket=destination_bucket_name, Key=file_name)
+    s.copy_object(CopySource=copy_object, Bucket=destination_bucket, Key=file_name)
     print("Helooo fromm  me")
